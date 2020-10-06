cb3ffe80a85a6db10caaf22ff5bf052b29efbc74
adelariestra <adelariestra@gmail.com>
2020-10-06 18:21:20 -0300

    rename

--- ruby/lib/BeforeAndAfter.rb
+++ ruby/lib/BeforeAndAfter.rb
@@ -47,7 +47,7 @@ module BeforeAndAfter
     #puts "!! DEBUG !! Nuevo metodo agregado:  #{nombre_metodo}"
     #puts self.instance_variables
 
-    mutex_subrescritura do
+    flag_subrescritura do
       # Método utilizado para evitar recursividad infinita
       metodo = instance_method(nombre_metodo) # Obtengo unbound method de la instancia con el nombre especificado
 
@@ -102,7 +102,7 @@ module BeforeAndAfter
     end
   end
 
-  def mutex_subrescritura
+  def flag_subrescritura
     # Para evitar tener un atributo con un nombre que podria existir en la clase, utilizo una "variable" del contexto del thread de ejecución
     return if Thread.current[:__actualizando__] # si ya esta overrideando skipeo
 
