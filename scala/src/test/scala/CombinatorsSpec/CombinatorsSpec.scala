9a1249db4b42968b4d6524093a46287022e8c562
adelariestra <adelariestra@gmail.com>
2020-10-29 17:14:06 -0300

    aprendimos notacion infija

--- scala/src/test/scala/CombinatorsSpec/CombinatorsSpec.scala
+++ scala/src/test/scala/CombinatorsSpec/CombinatorsSpec.scala
@@ -4,8 +4,28 @@ import org.scalatest.flatspec._
 import org.scalatest.matchers._
 import parsers._
 
-class ParserSpec extends AnyFlatSpec with should.Matchers {
-  it should "funcionar" in{
+class CombinatorsSpec extends AnyFlatSpec with should.Matchers {
+//  it should "parsear correctamente con combinator <|> matcheando con el primero" in{
+//    val aob = char('a') <|> char('b')
+//    Parsertest.ejecutarParserCombinado("arbol").get shouldEqual 'a':Char
+//  }
+//
+//  it should "parsear correctamente con combinator <|> matcheando con el segundo" in{
+//    val aob = char('a') <|> char('b')
+//    Parsertest.ejecutarParserCombinado("arbol").get shouldEqual 'a':Char
+//  }
+
+  it should "notacion infija" in {
+    class Test(val string:String){
+      def <|>(string2: Test): Unit ={
+        println("ASDASD" + string + string2.string)
+      }
+
+    }
+    val ola = new Test("test1")
+    val ola2 = new Test("test2")
+
+    ola <|> ola2
 
   }
 }
