b2bba1e33636ac093b2de24d3366a5c1c1c97ba4
adelariestra <adelariestra@gmail.com>
2020-11-11 17:30:08 -0300

    started with transf between siblings

--- scala/src/main/scala/simplificators/SimpleSimplificator.scala
+++ scala/src/main/scala/simplificators/SimpleSimplificator.scala
@@ -7,29 +7,91 @@ import scala.util.Try
 
 trait SimpleSimplificator extends (FigureTr => FigureTr)
 
-case object generalSimplificator extends SimpleSimplificator{
-  override def apply(mainFigure:FigureTr):FigureTr={
+case object generalSimplificator extends SimpleSimplificator {
+  override def apply(mainFigure: FigureTr): FigureTr = {
     // TODO: convert to superior order
-      mainFigure.applyFunction(nestedColorSimp)
-      // TODO: implement the rest of functions
+    mainFigure.applyFunction(nestedColorSimp)
+    // TODO: implement the rest of functions
   }
 }
 
+// TODO: implementation pending
+//case object transfBetweenSiblingsSimp extends SimpleSimplificator {
+//  override def apply(mainFigure: FigureTr): FigureTr = {
+//    simplify(mainFigure)
+//  }
+//
+//  def simplify(mainFigure: FigureTr): FigureTr = {
+//
+//    mainFigure match {
+//      // TODO: fix pattern matching
+//      case ColorTr(elementList, values) => {
+//        if (elementList.forall((element)=>element.equals(elementList(0)))){
+//          elementList(0) match {
+//            // TODO: fix pattern matching
+//            case ColorTr(_,valuesC) => {
+//              ColorTr(List(GroupFigure(elementList.map((element:ColorTr) => element.figuresContained(0)))), valuesC)
+//            }
+//            case EscalaTr(elementList, values) => {
+//              EscalaTr(elementList.map((element) => simplify(element)), values)
+//            }
+//            case RotacionTr(elementList, values) => {
+//              RotacionTr(elementList.map((element) => simplify(element)), values)
+//            }
+//            case TraslacionTr(elementList, values) => {
+//              TraslacionTr(elementList.map((element) => simplify(element)), values)
+//            }
+//            case GroupFigure(elementList) => {
+//              GroupFigure(elementList.map((element) => simplify(element)))
+//            }
+//          }
+//
+//        }
+//        ColorTr(elementList.map((element) => simplify(element)), values)
+//      }
+//      case EscalaTr(elementList, values) => {
+//        EscalaTr(elementList.map((element) => simplify(element)), values)
+//      }
+//      case RotacionTr(elementList, values) => {
+//        RotacionTr(elementList.map((element) => simplify(element)), values)
+//      }
+//      case TraslacionTr(elementList, values) => {
+//        TraslacionTr(elementList.map((element) => simplify(element)), values)
+//      }
+//      case GroupFigure(elementList) => {
+//        GroupFigure(elementList.map((element) => simplify(element)))
+//      }
+//      case a => a; // TODO: cambiar caso base?
+//    }
+//  }
+//}
 
-case object nestedColorSimp extends SimpleSimplificator{
-  override def apply(mainFigure:FigureTr):FigureTr={
+
+case object nestedColorSimp extends SimpleSimplificator {
+  override def apply(mainFigure: FigureTr): FigureTr = {
     simplify(mainFigure)
   }
 
-  def simplify(mainFigure:FigureTr): FigureTr ={
+  def simplify(mainFigure: FigureTr): FigureTr = {
     mainFigure match {
-      case ColorTr(List(ColorTr(contentList,a,b,c)),_,_,_) => ColorTr(contentList,a,b,c)
+      case ColorTr(List(ColorTr(contentList, a, b, c)), _, _, _) => ColorTr(contentList, a, b, c)
       // TODO: fix pattern matching
-      case ColorTr(elementList,values) =>{ColorTr(elementList.map((element)=>simplify(element)),values)}
-      case EscalaTr(elementList,values) =>{EscalaTr(elementList.map((element)=>simplify(element)),values)}
-      case RotacionTr(elementList,values) =>{RotacionTr(elementList.map((element)=>simplify(element)),values)}
-      case TraslacionTr(elementList,values) =>{TraslacionTr(elementList.map((element)=>simplify(element)),values)}
-      case GroupFigure(elementList) =>{GroupFigure(elementList.map((element)=>simplify(element)))}
+      case ColorTr(elementList, values) => {
+        ColorTr(elementList.map((element) => simplify(element)), values)
+      }
+      case EscalaTr(elementList, values) => {
+        EscalaTr(elementList.map((element) => simplify(element)), values)
+      }
+      case RotacionTr(elementList, values) => {
+        RotacionTr(elementList.map((element) => simplify(element)), values)
+      }
+      case TraslacionTr(elementList, values) => {
+        TraslacionTr(elementList.map((element) => simplify(element)), values)
+      }
+      case GroupFigure(elementList) => {
+        GroupFigure(elementList.map((element) => simplify(element)))
+      }
+      case a => a; // TODO: cambiar caso base?
     }
   }
 }
--- scala/src/test/scala/simplificators/SimplificatorsSpec.scala
+++ scala/src/test/scala/simplificators/SimplificatorsSpec.scala
@@ -11,8 +11,8 @@ class SimpleElementsSpec extends AnyFlatSpec with should.Matchers {
     color("color[200, 200, 200](\n   color[6, 6, 6](circulo[0 @ 5, 10])\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual ColorTr(List(CircleFigure(Position(0, 5), 10)), 6, 6, 6)
   }
 
-//  it should "simplificar correctamente transformaciones entre hijos" in {
-//    color("grupo(\n\tcolor[200, 200, 200](rectangulo[100 @ 100, 200 @ 200]),\n\tcolor[200, 200, 200](circulo[100 @ 300, 150])\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual ColorTr(List(CircleFigure(Position(0, 5), 10)), 6, 6, 6)
-//  }
+  it should "simplificar correctamente transformaciones entre hijos" in {
+    group("grupo(\n\tcolor[200, 200, 200](rectangulo[100 @ 100, 200 @ 200]),\n\tcolor[200, 200, 200](circulo[100 @ 300, 150])\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual color("color[200, 200, 200](\n   grupo(\n\trectangulo[100 @ 100, 200 @ 200],\n\tcirculo[100 @ 300, 150]\n   )\n)").get.elementoParseado
+  }
 
 }
