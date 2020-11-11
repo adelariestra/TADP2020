22cd66e5940e576377277a95769dc69fbb967378
adelariestra <adelariestra@gmail.com>
2020-11-11 15:10:55 -0300

    rotacion

--- scala/src/main/scala/elements/ComplexElements.scala
+++ scala/src/main/scala/elements/ComplexElements.scala
@@ -7,15 +7,24 @@ import scala.util.Try
 
 case object group extends Parser[GroupFigure] {
   override def apply(cadena: String): Try[ResultadoParseo[GroupFigure]] = {
-    val parseadorAux = string(",") <~ espacios
-    val parseadorGeneral = string("grupo(") <~ espacios <> figure.sepBy(parseadorAux) <~ espacios <~ char(')');
+    val parseadorGeneral = string("grupo") ~> groupContent;
     parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[(String, List[FigureTr])]) = {
-    ResultadoParseo(GroupFigure(result.elementoParseado._2), result.cadenaRestante)
+  def obtainFigure(result: ResultadoParseo[(List[FigureTr])]) = {
+    ResultadoParseo(GroupFigure(result.elementoParseado), result.cadenaRestante)
   }
 }
+
+case object groupContent extends Parser[List[FigureTr]] {
+  override def apply(cadena: String): Try[ResultadoParseo[List[FigureTr]]] = {
+    val parseadorAux = string(",") <~ espacios
+    val parseadorGeneral = char('(') ~> espacios ~> figure.sepBy(parseadorAux) <~ espacios <~ char(')');
+    parseadorGeneral.apply(cadena)
+  }
+}
+
+
 
 case object transformation extends Parser[TransformTr] {
   override def apply(cadena: String): Try[ResultadoParseo[TransformTr]] = {
@@ -27,8 +36,7 @@ case object transformation extends Parser[TransformTr] {
 
 case object escala extends Parser[EscalaTr] {
   override def apply(cadena: String): Try[ResultadoParseo[EscalaTr]] = {
-    val parseadorAux = string(",") <~ espacios
-    val parseadorGeneral = string("escala[") ~> double <~ string(", ") <> double <~ string("](") <~ espacios <> figure.sepBy(parseadorAux) <~ espacios <~ char(')');
+    val parseadorGeneral = string("escala[") ~> double <~ string(", ") <> double <~ string("]") <> groupContent;
     parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
   }
 
@@ -37,18 +45,18 @@ case object escala extends Parser[EscalaTr] {
   }
 }
 
-//
-//case object rotacion extends Parser[TransformTr] {
-//  override def apply(cadena: String): Try[ResultadoParseo[GroupFigure]] = {
-//    val parseadorAux = string(",") <~ espacios
-//    val parseadorGeneral = string("grupo(") <~ espacios <> figure.sepBy(parseadorAux) <~ espacios <~ char(')');
-//    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
-//  }
-//
-//  def obtainFigure(result: ResultadoParseo[(String, List[FigureTr])]) = {
-//    ResultadoParseo(GroupFigure(result.elementoParseado._2), result.cadenaRestante)
-//  }
-//}
+
+case object rotacion extends Parser[RotacionTr] {
+  override def apply(cadena: String): Try[ResultadoParseo[RotacionTr]] = {
+    val parseadorGeneral = string("rotacion[") ~> integer <~ string("]") <> groupContent;
+    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+  }
+
+  def obtainFigure(result: ResultadoParseo[((Int), List[FigureTr])]) = {
+    ResultadoParseo(RotacionTr(result.elementoParseado._2,result.elementoParseado._1), result.cadenaRestante)
+  }
+}
+
 //case object traslacion extends Parser[TransformTr] {
 //  override def apply(cadena: String): Try[ResultadoParseo[GroupFigure]] = {
 //    val parseadorAux = string(",") <~ espacios
--- scala/src/test/scala/elements/ComplexElementsSpec.scala
+++ scala/src/test/scala/elements/ComplexElementsSpec.scala
@@ -9,7 +9,7 @@ class ComplexElementsSpec extends AnyFlatSpec with should.Matchers {
     group("grupo(\n   \t triangulo[200 @ 50, 101 @ 335, 299 @ 335],\n   \t circulo[200 @ 350, 100]\n    )").get shouldEqual (ResultadoParseo(GroupFigure(List(TriangleFigure(Position(200, 50), Position(101, 335), Position(299, 335)), CircleFigure(Position(200, 350), 100))), ""))
   }
 
-  it should "parsear incorrectamente si algún elementoSimple falla" in {
+  it should "parsear incorrectamente un grupo si algún elementoSimple falla" in {
     group("grupo(\n   \t triangulo[200 @ 50, 101 @ 335],\n   \t circulo[200 @ 350, 100]\n    )").isFailure shouldEqual (true)
   }
 
@@ -19,5 +19,13 @@ class ComplexElementsSpec extends AnyFlatSpec with should.Matchers {
 
   it should "parsear correctamente una escala con más de un componente" in {
     escala("escala[6, 15](\n\tcirculo[0 @ 5, 10],\n\tcirculo[0 @ 5, 10]\n)").get shouldEqual (ResultadoParseo(EscalaTr(List(CircleFigure(Position(0, 5), 10),CircleFigure(Position(0, 5), 10)),6,15), ""))
+  }
+
+  it should "parsear correctamente una rotacion con un componente" in {
+    rotacion("rotacion[6](\n\tcirculo[0 @ 5, 10]\n)").get shouldEqual (ResultadoParseo(RotacionTr(List(CircleFigure(Position(0, 5), 10)),6), ""))
+  }
+
+  it should "parsear correctamente una rotacion con más de un componente" in {
+    rotacion("rotacion[6](\n\tcirculo[0 @ 5, 10],\n\tcirculo[0 @ 5, 10]\n)").get shouldEqual (ResultadoParseo(RotacionTr(List(CircleFigure(Position(0, 5), 10),CircleFigure(Position(0, 5), 10)),6), ""))
   }
 }
