d8c1b94cc15fd75bb0b599c1b3ce735f5633bba9
adelariestra <adelariestra@gmail.com>
2020-11-12 09:00:13 -0300

    code cleanup in complex elements

--- scala/src/main/scala/elements/ComplexElements.scala
+++ scala/src/main/scala/elements/ComplexElements.scala
@@ -1,17 +1,16 @@
 package elements
 
-import elements.circle.obtainFigure
 import parsers._
 
 import scala.util.Try
 
 case object group extends Parser[GroupFigure] {
   override def apply(cadena: String): Try[ResultadoParseo[GroupFigure]] = {
-    val parseadorGeneral = string("grupo") ~> groupContent;
-    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+    val parseadorGeneral = string("grupo") ~> groupContent
+    parseadorGeneral.apply(cadena).map(element => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[(List[FigureTr])]) = {
+  def obtainFigure(result: ResultadoParseo[List[FigureTr]]): ResultadoParseo[GroupFigure] = {
     ResultadoParseo(GroupFigure(result.elementoParseado), result.cadenaRestante)
   }
 }
@@ -19,15 +18,14 @@ case object group extends Parser[GroupFigure] {
 case object groupContent extends Parser[List[FigureTr]] {
   override def apply(cadena: String): Try[ResultadoParseo[List[FigureTr]]] = {
     val parseadorAux = string(",") <~ espacios
-    val parseadorGeneral = char('(') ~> espacios ~> figure.sepBy(parseadorAux) <~ espacios <~ char(')');
+    val parseadorGeneral = char('(') ~> espacios ~> figure.sepBy(parseadorAux) <~ espacios <~ char(')')
     parseadorGeneral.apply(cadena)
   }
 }
 
 case object figureContained extends Parser[FigureTr] {
   override def apply(cadena: String): Try[ResultadoParseo[FigureTr]] = {
-    val parseadorAux = string(",") <~ espacios
-    val parseadorGeneral = char('(') ~> espacios ~> figure <~ espacios <~ char(')');
+    val parseadorGeneral = char('(') ~> espacios ~> figure <~ espacios <~ char(')')
     parseadorGeneral.apply(cadena)
   }
 }
@@ -41,43 +39,43 @@ case object transformation extends Parser[TransformTr] {
 
 case object escala extends Parser[EscalaTr] {
   override def apply(cadena: String): Try[ResultadoParseo[EscalaTr]] = {
-    val parseadorGeneral = string("escala[") ~> double <~ string(", ") <> double <~ string("]") <> figureContained;
-    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+    val parseadorGeneral = string("escala[") ~> double <~ string(", ") <> double <~ string("]") <> figureContained
+    parseadorGeneral.apply(cadena).map(element => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[((Double,Double), FigureTr)]) = {
+  def obtainFigure(result: ResultadoParseo[((Double,Double), FigureTr)]): ResultadoParseo[EscalaTr] = {
     ResultadoParseo(EscalaTr(result.elementoParseado._2, result.elementoParseado._1._1, result.elementoParseado._1._2), result.cadenaRestante)
   }
 }
 
 case object rotacion extends Parser[RotacionTr] {
   override def apply(cadena: String): Try[ResultadoParseo[RotacionTr]] = {
-    val parseadorGeneral = string("rotacion[") ~> integer <~ string("]") <> figureContained;
-    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+    val parseadorGeneral = string("rotacion[") ~> integer <~ string("]") <> figureContained
+    parseadorGeneral.apply(cadena).map(element => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[((Int), FigureTr)]) = {
+  def obtainFigure(result: ResultadoParseo[(Int, FigureTr)]): ResultadoParseo[RotacionTr] = {
     ResultadoParseo(RotacionTr(result.elementoParseado._2,result.elementoParseado._1), result.cadenaRestante)
   }
 }
 
 case object traslacion extends Parser[TraslacionTr] {
   override def apply(cadena: String): Try[ResultadoParseo[TraslacionTr]] = {
-    val parseadorGeneral = string("traslacion[") ~> integer <~ string(", ") <> integer <~ string("]") <> figureContained;
-    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+    val parseadorGeneral = string("traslacion[") ~> integer <~ string(", ") <> integer <~ string("]") <> figureContained
+    parseadorGeneral.apply(cadena).map(element => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[((Int,Int), FigureTr)]) = {
+  def obtainFigure(result: ResultadoParseo[((Int,Int), FigureTr)]): ResultadoParseo[TraslacionTr] = {
     ResultadoParseo(TraslacionTr(result.elementoParseado._2,result.elementoParseado._1._1, result.elementoParseado._1._2), result.cadenaRestante)
   }
 }
 case object color extends Parser[ColorTr] {
   override def apply(cadena: String): Try[ResultadoParseo[ColorTr]] = {
-    val parseadorGeneral = string("color[") ~> integer <~ string(", ") <> integer <~ string(", ") <> integer <~ string("]") <> figureContained;
-    parseadorGeneral.apply(cadena).map((element) => obtainFigure(element))
+    val parseadorGeneral = string("color[") ~> integer <~ string(", ") <> integer <~ string(", ") <> integer <~ string("]") <> figureContained
+    parseadorGeneral.apply(cadena).map(element => obtainFigure(element))
   }
 
-  def obtainFigure(result: ResultadoParseo[(((Int,Int),Int), FigureTr)]) = {
+  def obtainFigure(result: ResultadoParseo[(((Int,Int),Int), FigureTr)]): ResultadoParseo[ColorTr] = {
     ResultadoParseo(ColorTr(result.elementoParseado._2,result.elementoParseado._1._1._1, result.elementoParseado._1._1._2,result.elementoParseado._1._2), result.cadenaRestante)
   }
 }
