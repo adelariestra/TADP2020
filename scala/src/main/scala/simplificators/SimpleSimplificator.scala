e14944f3997bc1236b7de472e73700d57a314ab5
adelariestra <adelariestra@gmail.com>
2020-11-12 10:41:20 -0300

    all nested simplif working

--- scala/src/main/scala/simplificators/SimpleSimplificator.scala
+++ scala/src/main/scala/simplificators/SimpleSimplificator.scala
@@ -7,21 +7,28 @@ trait SimpleSimplificator extends (FigureTr => FigureTr)
 case object generalSimplificator extends SimpleSimplificator {
   override def apply(mainFigure: FigureTr): FigureTr = {
     // TODO: convert to superior order
-    mainFigure.applyFunction(nestedColorSimp)
+    mainFigure.applyFunction(nestedTrSimp)
       .applyFunction(commonTrSimp)
     // TODO: implement the rest of functions
   }
 }
 
-case object nestedColorSimp extends SimpleSimplificator {
+case object nestedTrSimp extends SimpleSimplificator {
   override def apply(mainFigure: FigureTr): FigureTr = simplify(mainFigure)
 
   def simplify(mainFigure: FigureTr): FigureTr =
     mainFigure match {
-      case ColorTr(ColorTr(figureContained, a, b, c), _, _, _) => ColorTr(figureContained, a, b, c)
+      case ColorTr(ColorTr(figureContained, a1, a2, a3), _, _, _) => ColorTr(figureContained, a1, a2, a3)
+      case RotacionTr(RotacionTr(figureContained, a1), b1) => RotacionTr(figureContained, a1+b1)
+      case TraslacionTr(TraslacionTr(figureContained, a1, a2), b1, b2) => TraslacionTr(figureContained,a1+b1,a2+b2)
+      case EscalaTr(EscalaTr(figureContained, a1, a2), b1, b2) => EscalaTr(figureContained, a1*b1,a2*b2)
       case GroupFigure(elementList) => GroupFigure(elementList.map(element => simplify(element)))
       case anotherFigure: SimpleFigureTr => anotherFigure
-      case anotherFigure: TransformTr => anotherFigure
+
+      case EscalaTr(figure, val1, val2) => EscalaTr(simplify(figure), val1, val2)
+      case RotacionTr(figure, val1) => RotacionTr(simplify(figure), val1)
+      case TraslacionTr(figure, val1, val2) => TraslacionTr(simplify(figure), val1, val2)
+      case ColorTr(figure, val1, val2, val3) => ColorTr(simplify(figure), val1, val2, val3)
     }
 
 }
@@ -33,30 +40,30 @@ case object commonTrSimp extends SimpleSimplificator {
     mainFigure match {
       case GroupFigure(elementList) =>
         elementList.head match {
-          case EscalaTr(figure, values) =>
+          case EscalaTr(_, values) =>
             if (elementList.forall(element => element.equals(EscalaTr(element.figureContained, values))))
               EscalaTr(GroupFigure(elementList.map(element => element.figureContained)), values)
             else
               GroupFigure(elementList.map(element => simplify(element)))
 
-          case RotacionTr(figure, val1) =>
+          case RotacionTr(_, val1) =>
             if (elementList.forall(element => element.equals(RotacionTr(element.figureContained, val1))))
               RotacionTr(GroupFigure(elementList.map(element => element.figureContained)), val1)
             else
               GroupFigure(elementList.map(element => simplify(element)))
 
-          case TraslacionTr(figure, val1, val2) =>
+          case TraslacionTr(_, val1, val2) =>
             if (elementList.forall(element => element.equals(TraslacionTr(element.figureContained, val1, val2))))
               TraslacionTr(GroupFigure(elementList.map(element => element.figureContained)), val1, val2)
             else
               GroupFigure(elementList.map(element => simplify(element)))
 
-          case ColorTr(figure, val1, val2, val3) =>
+          case ColorTr(_, val1, val2, val3) =>
             if (elementList.forall(element => element.equals(ColorTr(element.figureContained, val1, val2, val3))))
               ColorTr(GroupFigure(elementList.map(element => element.figureContained)), val1, val2, val3)
             else
               GroupFigure(elementList.map(element => simplify(element)))
-              
+
           case GroupFigure(elementList) => GroupFigure(elementList.map(element => simplify(element)))
           case anotherFigure: SimpleFigureTr => anotherFigure
         }
--- scala/src/test/scala/simplificators/SimplificatorsSpec.scala
+++ scala/src/test/scala/simplificators/SimplificatorsSpec.scala
@@ -25,4 +25,16 @@ class SimpleElementsSpec extends AnyFlatSpec with should.Matchers {
     figure("grupo(\n\tcolor[215, 200, 200](rectangulo[100 @ 100, 200 @ 200]),\n\tcolor[200, 200, 200](circulo[100 @ 300, 150])\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual figure("grupo(\n\tcolor[215, 200, 200](rectangulo[100 @ 100, 200 @ 200]),\n\tcolor[200, 200, 200](circulo[100 @ 300, 150])\n)").get.elementoParseado
   }
 
+  it should "simplificar correctamente nested rotacion" in {
+    figure("rotacion[300](\n\trotacion[10](\n\t\trectangulo[100 @ 200, 300 @ 400]\n\t)\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual figure("rotacion[310](\n\trectangulo[100 @ 200, 300 @ 400]\n)").get.elementoParseado
+  }
+
+  it should "simplificar correctamente nested escala" in {
+    figure("escala[2, 3](\n      escala[3, 5](\n\t     circulo[0 @ 5, 10]\n      )\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual figure("escala[6, 15](\n\tcirculo[0 @ 5, 10]\n)").get.elementoParseado
+  }
+
+  it should "simplificar correctamente nested traslacion" in {
+    figure("traslacion[100, 5](\n\ttraslacion[20, 10](\n\t\tcirculo[0 @ 5, 10]\n)\n)").get.elementoParseado.applyFunction(generalSimplificator) shouldEqual figure("traslacion[120, 15](\n\tcirculo[0 @ 5, 10]\n)").get.elementoParseado
+  }
+
 }
